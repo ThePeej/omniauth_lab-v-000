1aad5814e5388969528257613955eebeb91f7cb7
PJ Frias <peejfrias@gmail.com>
2018-02-28 03:31:16 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228031949) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
